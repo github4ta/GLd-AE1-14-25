9100e493e72d600609174f527f6eb9a71f090537
IrynaT15 <irina.tainikova@gmail.com>
2025-10-30 20:01:55 +0100

    fixed main method

--- src/main/java/by/it_academy/tainikava/Tainikava.java
+++ src/main/java/by/it_academy/tainikava/Tainikava.java
@@ -2,6 +2,6 @@ package by.it_academy.tainikava;
 
 public class Tainikava {
     public static void main(String[] args) {
-        System.out.println("Hello, Team!");
+        System.out.print("Hello, Team!");
     }
 }
