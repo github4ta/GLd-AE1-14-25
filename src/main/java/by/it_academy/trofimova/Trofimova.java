c8077dd171bd34f99252f1a81083c72dba85c5a9
Maria Trofimova <maria.tarasowa@tut.by>
2025-10-30 21:55:39 +0300

    fix comment

--- src/main/java/by/it_academy/trofimova/Trofimova.java
+++ src/main/java/by/it_academy/trofimova/Trofimova.java
@@ -2,6 +2,6 @@ package by.it_academy.trofimova;
 
 public class Trofimova {
     public static void main(String[] args) {
-        System.out.println("Hello Team! :)))) ");
+        System.out.println("Hello Team!");
     }
 }
