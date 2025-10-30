3ac063394cd29ad3e4ced783a3d824d3867b1d9f
Katya Zarovskaya <KatyaZarovskaya@coherentsolutions.com>
2025-10-30 19:56:27 +0100

    fixed Zarouskaya.class

--- src/main/java/by/it_academy/zarouskaya/Zarouskaya.java
+++ src/main/java/by/it_academy/zarouskaya/Zarouskaya.java
@@ -2,6 +2,6 @@ package by.it_academy.zarouskaya;
 
 public class Zarouskaya {
     public static void main(String[] args) {
-        System.out.println("Hello Team!");
+        System.out.print("Hello Team!");
     }
 }
