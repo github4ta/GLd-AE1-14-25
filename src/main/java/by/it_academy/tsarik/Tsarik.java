c61f59c50e14085ca7fbbd66ac437daa348d9ab7
Sergei Tsarik <serge.tsarik@gmail.com>
2025-10-30 21:55:34 +0300

    fixed comment

--- src/main/java/by/it_academy/tsarik/Tsarik.java
+++ src/main/java/by/it_academy/tsarik/Tsarik.java
@@ -2,6 +2,6 @@ package by.it_academy.tsarik;
 
 public class Tsarik {
     public static void main(String[] args) {
-        System.out.println("Hello TEam!");
+        System.out.println("Hello Team!");
     }
 }
