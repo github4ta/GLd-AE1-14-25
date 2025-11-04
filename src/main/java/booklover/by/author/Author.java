e5f5b2806bcc81aa36c9209cdb87b58c9a7b284b
Katya Zarovskaya <KatyaZarovskaya@coherentsolutions.com>
2025-11-04 18:39:35 +0100

    add getter to Author

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -3,4 +3,12 @@ package booklover.by.author;
 public class Author {
     private String surname;
     private String name;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
