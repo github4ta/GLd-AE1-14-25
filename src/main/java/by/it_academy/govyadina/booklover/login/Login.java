256c5603a26bd400c7fbe539f08447d769f401f8
Irina <iragovyadina@mail.ru>
2025-11-04 12:34:39 +0300

    fixed comment

--- src/main/java/by/it_academy/govyadina/booklover/login/Login.java
+++ src/main/java/by/it_academy/govyadina/booklover/login/Login.java
@@ -1,18 +1,18 @@
 package by.it_academy.govyadina.booklover.login;
 
-import by.booklover.User;
 
-import static by.booklover.Users.users;
+import by.it_academy.govyadina.booklover.user.User;
+
+import static by.it_academy.govyadina.booklover.user.Users.users;
 
 public class Login {
-
     public void login(User user) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getPhoneNumber().contains(user.getPhoneNumber())) {
-                System.out.println("привет " + "Пользователь, " + user.getPhoneNumber());
+                System.out.println(String.format("привет Пользователь, %s", user.getPhoneNumber()));
             } else {
                 System.out.println("Вы не были зарегистрированы, мы вас зарегистрировали. Повторите вход.");
             }
         }
-        }
     }
+}
