21ed8e79dd00dceb818227b7255e8585a09f1cbb
Viktor Tretyak <nay40k777@gmail.com>
2025-11-04 21:32:44 +0300

    resolved conflict

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -17,21 +17,11 @@ public class Author {
         this.name = name;
     }
 
-    public Author(String surname, String name) {
-        this.surname = surname;
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
     }
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s %s", name, surname);
     }
 }
