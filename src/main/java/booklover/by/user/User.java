ff26882cf8399d4e38008aef382fd86390e5a06d
Irina <iragovyadina@mail.ru>
2025-11-04 21:54:43 +0300

    add user constructor

--- src/main/java/booklover/by/user/User.java
+++ src/main/java/booklover/by/user/User.java
@@ -3,4 +3,9 @@ package booklover.by.user;
 public class User {
     private String phoneNumber;
     private int password;
+
+    public User(String phoneNumber, int password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
 }
