7a32e59c5128f449bfde0caea636f11266305f25
Dima <Dzmitry3077@gmail.com>
2025-11-04 21:54:11 +0300

    add setter to User

--- src/main/java/booklover/by/user/User.java
+++ src/main/java/booklover/by/user/User.java
@@ -3,4 +3,12 @@ package booklover.by.user;
 public class User {
     private String phoneNumber;
     private int password;
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
