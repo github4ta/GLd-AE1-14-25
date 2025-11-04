ed98356429ff9f037f607cdd0bc3d2c87f49b68c
Maryna Haranskaya <mari_na_s@inbox.ru>
2025-11-04 21:43:45 +0300

    -fix conflict

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -10,18 +10,6 @@ public class Author {
         this.surname = surname;
         this.name = name;
     }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Author(String surname, String name) {
-        this.surname = surname;
-        this.name = name;
-    }
 
     public String getSurname() {
         return surname;
