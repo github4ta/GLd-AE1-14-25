8ec417a7b6864d0077e215f2556e31cc0d4adc06
Tamara <vovktamars@gmail.com>
2025-11-04 20:40:27 +0300

    add setter

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -3,4 +3,12 @@ package booklover.by.author;
 public class Author {
     private String surname;
     private String name;
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
