879f6d5c3b3be06b9936f00eb6c258575b0ebcd1
Viktor Tretyak <158050592+nay40k@users.noreply.github.com>
2025-11-04 21:33:02 +0300

    add toString method (override) (#26)
    * add toString method (override)
    
    * resolved conflict

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -17,11 +17,6 @@ public class Author {
         this.name = name;
     }
 
-    public Author(String surname, String name) {
-        this.surname = surname;
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
     }
