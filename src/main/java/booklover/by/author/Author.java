be8618fe70f961734cd7abf4addd351936e5e539
Dima <Dzmitry3077@gmail.com>
2025-11-04 20:42:39 +0300

    add equals & hash code to Author

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -1,6 +1,20 @@
 package booklover.by.author;
 
+import java.util.Objects;
+
 public class Author {
     private String surname;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(surname, author.surname) && Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, name);
+    }
 }
