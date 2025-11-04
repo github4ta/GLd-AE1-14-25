1edd3cbc003fdb26fc592e07e2db999510286d27
Viktor Tretyak <nay40k777@gmail.com>
2025-11-04 20:41:57 +0300

    add toString method (override)

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -3,4 +3,9 @@ package booklover.by.author;
 public class Author {
     private String surname;
     private String name;
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", name, surname);
+    }
 }
