982dc4a71e2ab1ef257f95aeb8eed9734a1d7319
Katya Zarovskaya <KatyaZarovskaya@coherentsolutions.com>
2025-11-04 19:55:33 +0100

    add toString to User

--- src/main/java/booklover/by/user/User.java
+++ src/main/java/booklover/by/user/User.java
@@ -3,4 +3,12 @@ package booklover.by.user;
 public class User {
     private String phoneNumber;
     private int password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", password=" + password +
+                '}';
+    }
 }
