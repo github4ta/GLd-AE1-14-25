b63d91d24d3b9567cb2d8357a2dc01515358849d
IrynaT15 <irina.tainikova@gmail.com>
2025-11-04 19:59:42 +0100

    add empty constructor for Cart

--- src/main/java/booklover/by/cart/Cart.java
+++ src/main/java/booklover/by/cart/Cart.java
@@ -3,9 +3,14 @@ package booklover.by.cart;
 import booklover.by.book.Book;
 import booklover.by.user.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     private List<Book> books;
     private User user;
+
+    public Cart() {
+        this.books = new ArrayList<>();
+    }
 }
