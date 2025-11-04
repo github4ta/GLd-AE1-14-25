d500a0b4f59a5d6428778a9b50616f09abd23e4f
Maryna Haranskaya <mari_na_s@inbox.ru>
2025-11-04 21:57:38 +0300

    -add constructor with user to Cart

--- src/main/java/booklover/by/cart/Cart.java
+++ src/main/java/booklover/by/cart/Cart.java
@@ -3,9 +3,15 @@ package booklover.by.cart;
 import booklover.by.book.Book;
 import booklover.by.user.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     private List<Book> books;
     private User user;
+
+    public Cart(User user) {
+        this.user = user;
+        this.books = new ArrayList<>();
+    }
 }
