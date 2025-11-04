e86ca57a39c4e18f88e019c376058da88f4113dc
NadezhdaSinkevich <n.sinkevich@rambler.ru>
2025-11-05 01:35:04 +0300

    fix after review

--- src/main/java/by/it_academy/sinkevich/Sinkevich.java
+++ src/main/java/by/it_academy/sinkevich/Sinkevich.java
@@ -1,8 +1,5 @@
 package by.it_academy.sinkevich;
-import by.it_academy.sinkevich.bookLover.Demoshop;
 
 public class Sinkevich {
-    public static void main (String[] args){
-        Demoshop.demoshop();
-    }
+
 }
