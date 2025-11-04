fd44fb5ed3c103a128a24099a246e516550a6348
Maria Trofimova <maria.tarasowa@tut.by>
2025-11-04 20:42:20 +0300

    add set to Book

--- src/main/java/booklover/by/book/Book.java
+++ src/main/java/booklover/by/book/Book.java
@@ -9,4 +9,28 @@ public class Book {
     private String publisher;
     private String isbn;
     private double price;
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
