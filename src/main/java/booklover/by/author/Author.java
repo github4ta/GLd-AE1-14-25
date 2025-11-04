75011f9c65c1c3d81cecc42967eb27337069e782
Maryna Haranskaya <mari_na_s@inbox.ru>
2025-11-04 21:36:24 +0300

    Revert "-fix conflict"
    This reverts commit e2b0d27395c19c13ef6aef7c9d6042557437ab6a.

--- src/main/java/booklover/by/author/Author.java
+++ src/main/java/booklover/by/author/Author.java
@@ -17,6 +17,11 @@ public class Author {
         this.name = name;
     }
 
+    public Author(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
     public String getSurname() {
         return surname;
     }
