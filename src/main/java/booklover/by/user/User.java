f7b089449e3dcba16eacc0fa16e909ed01f9592f
Viktor Tretyak <nay40k777@gmail.com>
2025-11-04 21:56:43 +0300

    add getters to User class

--- src/main/java/booklover/by/user/User.java
+++ src/main/java/booklover/by/user/User.java
@@ -3,4 +3,13 @@ package booklover.by.user;
 public class User {
     private String phoneNumber;
     private int password;
+
+
+    public int getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
