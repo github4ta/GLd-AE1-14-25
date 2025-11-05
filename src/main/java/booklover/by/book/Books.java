e030a85c9be73614684fdb7a99ecbd86f608f2b7
Tamara <vovktamars@gmail.com>
2025-11-05 19:50:20 +0300

    add method to Books - filterBooksByAuthor-updated version

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Books {
 
     public static List<Book> filterBooksByAuthor(List<Book> books, String authorSurname) {
-        List<Book> filteredBooksResult = new ArrayList<>();
+        List<Book> filteredBooksByAuthorResult = new ArrayList<>();
         for (int i = 0; i < books.size(); i++) {
             Book book = books.get(i);
             if (books.get(i).getAuthor().getSurname().equals(authorSurname)) {
-                filteredBooksResult.add(book);
+                filteredBooksByAuthorResult.add(book);
             }
         }
-        return filteredBooksResult;
+        return filteredBooksByAuthorResult;
     }
 }
