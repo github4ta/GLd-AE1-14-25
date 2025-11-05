90fe5da36d443d4593411224581c2c6d9e8aad95
IrinaGovyadina <iragovyadina@mail.ru>
2025-11-05 22:03:47 +0300

    Bklv UI 002BKLV_UL-002-Books- filterBooksByPublisher (#50)
    * add books filterBooksByPublisher

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -2,25 +2,17 @@ package booklover.by.book;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Comparator;
-import java.util.stream.Collectors;
+
 
 public class Books {
-    public static List<Book> filterBooksByIsbn(List<Book> books, String isbn) {
-        List<Book> newBooks = new ArrayList<>();
+
+    public static List<Book> filterBooksByPublisher(List<Book> books, String publisher) {
+        List<Book> filtedBooksByPublisher = new ArrayList<>();
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i).getIsbn() == isbn) {
-                newBooks.add(books.get(i));
+            if (books.get(i).getPublisher().equals(publisher)) {
+                filtedBooksByPublisher.add(books.get(i));
             }
         }
-        return newBooks;
-    }
-
-    public static List<Book> filterBooksByPublisherAndYear(List<Book> books, String publisher, int year) {
-        return books.stream()
-                .filter(book -> book.getPublisher().equalsIgnoreCase(publisher)
-                        && book.getYear() == year)
-                .sorted(Comparator.comparing(Book::getPrice))
-                .collect(Collectors.toList());
+        return filtedBooksByPublisher;
     }
 }
