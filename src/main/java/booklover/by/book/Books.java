712229f186073cc2efc384acd6561605dfcf28ee
Irina <iragovyadina@mail.ru>
2025-11-05 20:46:58 +0300

    add books filterBooksByPublisher

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -1,4 +1,16 @@
 package booklover.by.book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Books {
+    public static List<Book> filterBooksByPublisher(List<Book> books, String publisher) {
+        List<Book> booksfilteredByPublisher = new ArrayList<>();
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getPublisher().equals(publisher)) {
+                booksfilteredByPublisher.add(books.get(i));
+            }
+        }
+        return booksfilteredByPublisher;
+    }
 }
