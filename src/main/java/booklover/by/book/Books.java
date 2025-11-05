70efafd9abf8fa07cfab55ea5c4c3ed95808e235
KatyaZarouskaya <usataya@icloud.com>
2025-11-05 19:35:12 +0100

    BKLV_UI-005 - Books - filterBooksByPublisherAndYear (#48)
    * BKLV_UI-005/added filterBooksByPublisherAndYear to Books/conflict resolved
    
    ---------
    
    Co-authored-by: Katya Zarovskaya <KatyaZarovskaya@coherentsolutions.com>

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -2,6 +2,8 @@ package booklover.by.book;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Books {
     public static List<Book> filterBooksByIsbn(List<Book> books, String isbn) {
@@ -12,5 +14,13 @@ public class Books {
             }
         }
         return newBooks;
+    }
+
+    public static List<Book> filterBooksByPublisherAndYear(List<Book> books, String publisher, int year) {
+        return books.stream()
+                .filter(book -> book.getPublisher().equalsIgnoreCase(publisher)
+                        && book.getYear() == year)
+                .sorted(Comparator.comparing(Book::getPrice))
+                .collect(Collectors.toList());
     }
 }
