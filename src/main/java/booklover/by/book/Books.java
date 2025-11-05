2ea7ef0d5dbb64afd372e8d86e8977252c4657ab
MarynaHaranskaya <mari_na_s@inbox.ru>
2025-11-05 21:30:53 +0300

    -add method filterBooksByIsbn to Book (#49)

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -1,4 +1,16 @@
 package booklover.by.book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Books {
+    public static List<Book> filterBooksByIsbn(List<Book> books, String isbn) {
+        List<Book> newBooks = new ArrayList<>();
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getIsbn() == isbn) {
+                newBooks.add(books.get(i));
+            }
+        }
+        return newBooks;
+    }
 }
