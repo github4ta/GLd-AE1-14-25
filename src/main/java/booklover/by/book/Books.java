65f18cd60e082e33bee51b3076c95a94c1c7a916
Katya Zarovskaya <KatyaZarovskaya@coherentsolutions.com>
2025-11-05 19:12:05 +0100

    BKLV_UI-005/added filterBooksByPublisherAndYear to Books

--- src/main/java/booklover/by/book/Books.java
+++ src/main/java/booklover/by/book/Books.java
@@ -1,4 +1,15 @@
 package booklover.by.book;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Books {
+    public static List<Book> filterBooksByPublisherAndYear(List<Book> books, String publisher, int year) {
+        return books.stream()
+                .filter(book -> book.getPublisher().equalsIgnoreCase(publisher)
+                        && book.getYear() == year)
+                .sorted(Comparator.comparing(Book::getPrice))
+                .collect(Collectors.toList());
+    }
 }
