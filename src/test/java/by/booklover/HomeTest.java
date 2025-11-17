78bb2e36ba38ce10403c2f0e4166cff7e0cdee14
NadezhdaSinkevich <n.sinkevich@rambler.ru>
2025-11-17 22:23:21 +0300

    testDelivery() add

--- src/test/java/by/booklover/HomeTest.java
+++ src/test/java/by/booklover/HomeTest.java
@@ -16,7 +16,9 @@ public class HomeTest {
     private final String COOKIE_ALERT_CLOSE = "//span[@id='js-cookie-alert-close']";
 
     private WebDriver driver;
+
     // продолжаем добавлять ваши XPath'ы для веб-элементов
+    private String deliveryUrl = "//li[@class='nav__item']/a[@href='/delivery/']";
 
     @BeforeEach
     public void setupDriverAndOpenHomePageAndCloseCookieAlert() {
@@ -32,6 +34,12 @@ public class HomeTest {
     public void testBasket() {
         WebElement elementBasket = driver.findElement(By.xpath("//span[@class='user-link__text']"));
         Assertions.assertEquals("Корзина", elementBasket.getText());
+    }
+
+    @Test
+    public void testDelivery(){
+        WebElement element = driver.findElement(By.xpath(deliveryUrl));
+        Assertions.assertEquals("Доставка",element.getText());
     }
 
     @AfterEach
