b47122899f76c050b394c4db0513a83602602b6d
MarynaHaranskaya <mari_na_s@inbox.ru>
2025-12-04 11:28:52 +0300

    BKLV_TEST_UI-0102 – HomeTest – Строка поиска

--- src/test/java/by/booklover/HomeTest.java
+++ src/test/java/by/booklover/HomeTest.java
@@ -1,5 +1,6 @@
 package by.booklover;
 
+import by.booklover.utils.Waits;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -73,6 +74,15 @@ public class HomeTest {
     @Test
     public void testPayment() {
         Assertions.assertEquals(homePage.PAYMENT_URL_LABEL, homePage.getPaymentUrlText());
+    }
+
+    @Test
+    public void testSearchString(){
+        WebElement searchForm = driver.findElement(By.xpath("//form[@class='header__search-form']"));
+        String textInSearch = "//input[@id='search']";
+        Assertions.assertTrue(driver.findElement(By.xpath(textInSearch)).isDisplayed());
+        String getAttribute = driver.findElement(By.xpath(textInSearch)).getAttribute("placeholder");
+        Assertions.assertEquals("Поиск среди 290 000 книг, введите автора, название, isbn, серию или другое...",getAttribute);
     }
 
     @AfterEach
