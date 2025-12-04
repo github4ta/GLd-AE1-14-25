c1bc214652d27ec108425665f9ce9cac6fe144b7
Sergei Tsarik <serge.tsarik@gmail.com>
2025-12-04 11:59:39 +0300

    refactor code

--- src/main/java/by/booklover/pages/HomePage.java
+++ src/main/java/by/booklover/pages/HomePage.java
@@ -1,4 +1,4 @@
-package by.booklover.pages.home;
+package by.booklover.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/test/java/by/booklover/HomeTest.java
+++ src/test/java/by/booklover/HomeTest.java
@@ -1,6 +1,6 @@
 package by.booklover;
 
-import by.booklover.pages.home.HomePage;
+import by.booklover.pages.HomePage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
