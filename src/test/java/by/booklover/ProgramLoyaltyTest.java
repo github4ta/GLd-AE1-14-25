8d45a33ba6cf3924b49a88570cf45a8930293c74
Maria Trofimova <maria.tarasowa@tut.by>
2025-12-10 13:49:32 +0300

    add testDiscountTableIsDisplayed and verifySumAndDiscounts in ProgramLoyaltyTest

--- src/test/java/by/booklover/ProgramLoyaltyTest.java
+++ src/test/java/by/booklover/ProgramLoyaltyTest.java
@@ -2,11 +2,15 @@ package by.booklover;
 
 import by.booklover.pages.HomePage;
 import by.booklover.pages.ProgramLoyaltyPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ProgramLoyaltyTest {
 
@@ -28,12 +32,34 @@ public class ProgramLoyaltyTest {
     }
 
     @Test
-    public void checkLoyaltyProgramPageUrl() {
+    public void verifyLoyaltyProgramPageUrl() {
         Assertions.assertEquals(programLoyaltyPage.PROGRAM_LOYALTY_URL, driver.getCurrentUrl());
     }
 
     @Test
-    public void checkLoyaltyProgramTitle() {
+    public void verifyLoyaltyProgramTitle() {
         Assertions.assertEquals(programLoyaltyPage.PROGRAM_LOYALTY_LABEL, programLoyaltyPage.getProgramLoyaltyTitleText());
+    }
+
+    @Test
+    public void testDiscountTableIsDisplayed() {
+        Assertions.assertTrue(programLoyaltyPage.isDiscountTableDisplayed(), "Discount Table on the page is not displayed");
+    }
+
+    @Test
+    public void verifySumAndDiscounts () {
+        List<WebElement> rows = programLoyaltyPage.getTableRowsExceptForTitle();
+        for (int i = 0; i < rows.size(); i++) {
+            String actualSum = programLoyaltyPage.getSumFromRow(rows.get(i));
+            String actualDiscount = programLoyaltyPage.getDiscountFromRow(rows.get(i));
+
+            Assertions.assertEquals(programLoyaltyPage.EXPECTED_SUMS.get(i), actualSum);
+            Assertions.assertEquals(programLoyaltyPage.EXPECTED_DISCOUNTS.get(i), actualDiscount);
+        }
+    }
+
+    @AfterEach
+    public void quitDriver() {
+        driver.quit();
     }
 }
