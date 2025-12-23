9647608af456987d7c460c842e52576b7a61fbae
Maryna Haranskaya <mari_na_s@inbox.ru>
2025-12-23 21:16:03 +0300

    add testPostLoginKufar

---
+++ src/test/java/by/booklover/ApiTest.java
@@ -0,0 +1,55 @@
+package by.booklover;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+    public class ApiTest {
+        @Test
+        public void testBeHomePage() {
+            String url ="https://booklover.by/";
+            when().get(url).then().statusCode(200);
+        }
+
+        @Test
+        public void testPostLogin() {
+            String url ="https://booklover.by/personal/profile/?login=yes";
+            given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("AUTH_FORM", "Y")
+                    .formParam("TYPE", "Y")
+                    .formParam("USER_REMEMBER", "Y")
+                    .formParam("backurl", "/personal/profile/")
+                    .formParam("USER_REMEMBER", "Y")
+                    .formParam("USER_LOGIN", "+375 29 788-2196") // Rest Assured сам закодирует пробел
+                    .formParam("USER_PASSWORD", "jkbj") // Rest Assured сам закодирует пробел
+                    .when()
+                    .post(url)
+                    .then().assertThat().statusCode(200);
+        }
+
+        @Test
+        public void testPostLoginKufar() {
+            String url ="https://cre-auth.kufar.by/v2/auth/signin?token_type=user";
+            String body = """
+    {
+    "login": "marig_na_s@inbox.ru",
+    "password": "cgfcgfc",
+    "recaptcha_user_response": "",
+    "recaptcha_secret_version": "v1",
+    "recaptcha_platform": "web"
+}
+                    """;
+            given()
+                    .header("x-device-id","645eb5ac9a97eb9b8a4998305f6c5733")
+                    .header("x-app-name","Web Kufar")
+                    .body(body)
+            .when()
+                    .post(url)
+            .then()
+                    .log()
+                    .all()
+                    .statusCode(401);
+        }
+    }
