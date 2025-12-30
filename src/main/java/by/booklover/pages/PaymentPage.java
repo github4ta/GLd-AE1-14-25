894cdf5e2853b36d5372821e3845a416069284c3
Viktor Tretyak <158050592+nay40k@users.noreply.github.com>
2025-12-30 15:28:27 +0300

    BKLV_REFC_UI-0115 – PaymentTest (#82)
    * refactor code PaymentPage + PaymentTest
    
    * Update PaymentTest.java

--- src/main/java/by/booklover/pages/PaymentPage.java
+++ src/main/java/by/booklover/pages/PaymentPage.java
@@ -1,22 +1,18 @@
 package by.booklover.pages;
 
+import by.booklover.driver.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class PaymentPage {
 
-    private final WebDriver driver;
-    private final String EXPECTED_URL = "https://booklover.by/payment/";
-    private final String EXPECTED_TITLE = "Оплата";
+    private final org.openqa.selenium.WebDriver driver;
 
     private final By CASH_PAYMENT_INFO = By.xpath("//tr[td[contains(text(), 'Наличные')] and td[contains(text(), 'Октябрьская площадь')]]");
     private final By CARD_PAYMENT_INFO = By.xpath("//tr[td[contains(text(), 'Банковской картой')] and td[contains(text(), 'Октябрьская площадь')]]");
-
     private final By PAGE_HEADING = By.tagName("h1");
 
-    public PaymentPage(WebDriver driver) {
-        this.driver = driver;
+    public PaymentPage() {
+        this.driver = Driver.getDriver();
     }
 
     public String getCurrentUrl() {
@@ -33,13 +29,5 @@ public class PaymentPage {
 
     public String getCardPaymentInfoText() {
         return driver.findElement(CARD_PAYMENT_INFO).getText();
-    }
-
-    public boolean isCashInfoDisplayed() {
-        return driver.findElement(CASH_PAYMENT_INFO).isDisplayed();
-    }
-
-    public boolean isCardInfoDisplayed() {
-        return driver.findElement(CARD_PAYMENT_INFO).isDisplayed();
     }
 }
