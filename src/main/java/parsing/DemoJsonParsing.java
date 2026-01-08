556ead2bf73dacaab864511636197d03d10863f0
Maria Trofimova <maria.tarasowa@tut.by>
2026-01-08 20:06:05 +0300

    add Parsing JSON

---
+++ src/main/java/parsing/DemoJsonParsing.java
@@ -0,0 +1,46 @@
+package parsing;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class DemoJsonParsing {
+    public static void main(String[] args) {
+        String json = """ 
+                    {
+                    "errors": [
+                        {
+                            "message": "400: Bad Request",
+                            "locations": [
+                                {
+                                    "line": 2,
+                                    "column": 3
+                                }
+                            ],
+                            "path": [
+                                "passwordLogin"
+                            ],
+                            "extensions": {
+                                "errors": {
+                                    "ui": [
+                                        {
+                                            "id": 4000006,
+                                            "text": "Incorrect email or password. Please enter them again.",
+                                            "type": "error"
+                                        }
+                                    ]
+                                },
+                                "code": "BAD_REQUEST"
+                            }
+                        }
+                    ],
+                    "data": null
+                }
+                """;
+
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+
+        String message = jsonObject.getAsJsonArray("errors").get(0).getAsJsonObject().get("message").toString();
+
+        System.out.println(message);
+    }
+}
