17c9a2b1b5fecec677818f05420fd18ffe404ace
Dima <Dzmitry3077@gmail.com>
2026-01-08 22:01:00 +0300

    add parsing html examples

---
+++ src/main/java/parsing/DemoHtmlParsing.java
@@ -0,0 +1,22 @@
+package parsing;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import static io.restassured.RestAssured.given;
+
+public class DemoHtmlParsing {
+    public static void main(String[] args) {
+        String html = given().queryParam("q", "qwerty")
+                .get("https://booklover.by/search/")
+                .then().extract().body().asString();
+
+        Document doc = Jsoup.parse(html);
+        Elements elements = doc.select("h2.not-found__title");
+        Element element = elements.get(0);
+        System.out.println(element.text());
+
+    }
+}
