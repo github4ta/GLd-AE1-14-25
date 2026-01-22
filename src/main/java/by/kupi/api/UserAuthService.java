905401fb08a25c396a454f9152953666dafec6fd
Maryna Haranskaya <mari_na_s@inbox.ru>
2026-01-22 20:29:16 +0300

    refactor code

--- src/main/java/by/kupi/api/UserAuthService.java
+++ src/main/java/by/kupi/api/UserAuthService.java
@@ -8,16 +8,9 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class UserAuthService {
-    //адрес
-    //хедеры
-    //боди
     private String UrlUserAuth = "https://kupi.by/user/auth";
-    private Map<String, String> QueryParams;
-    private Map<String, String> Headers;
 
     private Response response;
-
-    private String body;
 
     public void doRequest() {
         response = given()
@@ -28,43 +21,25 @@ public class UserAuthService {
                 .post(UrlUserAuth);
     }
 
-    public void printResponse(){
+    public void printResponse() {
         response.then()
                 .log().all();
     }
 
-    private Map<String, String> getQueryParams(){
+    private Map<String, String> getQueryParams() {
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("t", "1768934294587");
         return queryParams;
     }
 
-    private Map<String, String> getHeaders () {
+    private Map<String, String> getHeaders() {
         Map<String, String> headers = new HashMap<>();
         headers.put("content-type", "application/json; charset=utf-8");
         headers.put("x-requested-with", "XMLHttpRequest");
-        return  headers;
+        return headers;
     }
 
-    private void setQueryParams(){
-        QueryParams = new HashMap<>();
-        QueryParams.put("t", "1768934294587");
-    }
-
-    private void setHeaders () {
-        Map<String, String> headers = new HashMap<>();
-        Headers.put("content-type", "application/json; charset=utf-8");
-        Headers.put("x-requested-with", "XMLHttpRequest");
-    }
-
-    public UserAuthService() {
-        setQueryParams();
-        setHeaders();
-        setBody();
-
-    }
-
-    private String getBody(){
+    private String getBody() {
         String body = """
                 {
                     "login": "login",
@@ -75,17 +50,5 @@ public class UserAuthService {
                 }
                 """;
         return body;
-    }
-
-    private void setBody(){
-         body = """
-                {
-                    "login": "login",
-                    "type": "email_password",
-                    "email": "it-test20012026@gmail.com",
-                    "password": "Password!234",
-                    "_token": "PV07i5GQ4yeGJG88IP81Fn2Ks1TiabPh1gWKkhNE"
-                }
-                """;
     }
 }
--- src/test/java/by/kupi/api/LoginTest.java
+++ src/test/java/by/kupi/api/LoginTest.java
@@ -1,34 +1,17 @@
 package by.kupi.api;
 
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 public class LoginTest {
     @Test
     public void testLogin() {
-        String body = """
-                {
-                    "login": "login",
-                    "type": "email_password",
-                    "email": "it-test20012026@gmail.com",
-                    "password": "Password!234",
-                    "_token": "PV07i5GQ4yeGJG88IP81Fn2Ks1TiabPh1gWKkhNE"
-                }
-                """;
-        Response response = given().baseUri("https://kupi.by")
-                .queryParam("t", "1768934294587")
-                .header("content-type", "application/json; charset=utf-8")
-                .header("x-requested-with", "XMLHttpRequest")
-                .body(body)
-        .when()
-                .post("/user/auth");
+        UserAuthService userAuthService = new UserAuthService();
+        userAuthService.doRequest();
+        userAuthService.printResponse();
 
-        response.then()
+        /*response.then()
                 .log().all()
                 .statusCode(422)
-                .body("message", equalTo("Выбранное значение для E-Mail адрес некорректно."));
+                .body("message", equalTo("Выбранное значение для E-Mail адрес некорректно."));*/
     }
 }
