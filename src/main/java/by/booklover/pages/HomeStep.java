079528ae5e0d5e080025db898d5b6bcceb431086
MarynaHaranskaya <mari_na_s@inbox.ru>
2026-01-29 21:53:29 +0300

    Merge pull request #106
    * Add Cucumber

---
+++ src/main/java/by/booklover/pages/HomeStep.java
@@ -0,0 +1,21 @@
+package by.booklover.pages;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
+
+public class HomeStep {
+    private HomePage homePage;
+
+    @Given("User opens Home page")
+    public void openAndCloseCookie(){
+        homePage = new HomePage();
+        homePage.open();
+        homePage.clickCookieAlertClose();
+    }
+
+    @Then("Basket button text is matched")
+    public void verifyThatBasketButtonTextIsMatched(){
+        Assertions.assertEquals(homePage.BASKET_LABEL, homePage.getBasketButtonText());
+    }
+}
---
+++ src/test/resources/features/home.feature
@@ -0,0 +1,5 @@
+Feature: Home page test-cases.
+
+  Scenario: Verify basket button text
+    Given User opens Home page
+    Then Basket button text is matched
