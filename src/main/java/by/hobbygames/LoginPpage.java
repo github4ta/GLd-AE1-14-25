c1e2a33cc0c2e6d9793bc5d72ac67bf37909ef1e
Sergei Tsarik <serge.tsarik@gmail.com>
2026-02-04 09:58:52 +0300

    refactor code with wait

--- src/main/java/by/hobbygames/LoginPpage.java
+++ src/main/java/by/hobbygames/LoginPpage.java
@@ -2,19 +2,31 @@ package by.hobbygames;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPpage {
     public By inputLoginPath = By.xpath("//input[@name='login']");
     public By inputPasswordPath = By.xpath("//input[@name='password']");
     public By buttonLogin = By.xpath("//input[@type='submit']");
 
+    private String INPUT_LOGIN = "//input[@name='login']";
+    private String ERROR_INPUT_LOGIN = INPUT_LOGIN + "/following-sibling::div";
+
     public By messageErrorLoginPath = By.xpath("//label[@data-scenario='login']/div[@class='error']");
     public By messageErrorPasswordPath = By.xpath("//label[@class='password-group']/div[@class='error']");
+
+    private final By LOGIN_POPUP = By.xpath("//div[@class='login-popup']");
 
     public WebDriver driver;
 
     public LoginPpage() {
-        this.driver = Driver.getDriver();
+        driver = Driver.getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LOGIN_POPUP));
     }
 
     public void inputLogin(String login){
@@ -30,10 +42,14 @@ public class LoginPpage {
     }
 
     public String getTextMessageErrorLogin(){
-        return driver.findElement(messageErrorLoginPath).getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        WebElement messageErrorLogin = wait.until(ExpectedConditions.visibilityOfElementLocated(messageErrorLoginPath));
+        return messageErrorLogin.getText();
     }
 
     public String getTextMessageErrorPasword(){
-        return driver.findElement(messageErrorPasswordPath).getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        WebElement messageErrorPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(messageErrorPasswordPath));
+        return messageErrorPassword.getText();
     }
 }
