de6cb1b546ac2af8f35a4c75d20ba8c2daff0c7c
Iryna <irina.tainikova@gmail.com>
2026-02-10 20:24:45 +0100

    code refactor for Playwright

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,31 +1,47 @@
-import com.codeborne.selenide.*;
+import com.microsoft.playwright.*;
 import org.testng.*;
 import org.testng.annotations.*;
 
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
-
 public class LoginTest {
+    static Playwright playwright;
+    static Browser browser;
 
-    @BeforeSuite
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
     public void testLogin() {
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 5000;
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
 
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
     }
 
     @Test
     public void testInvalidCredential() {
-        Selenide.open("https://chitatel.by/");
-        $(byXpath("//div[@class='h-login']//a[@class='block__link login__link login_popup']//div[@class ='name']")).click();
-        $(byXpath("//a[contains(@class,'_js-tab-btn') and normalize-space()='Email']")).click();
-        $(byXpath("//input[@type='email' and @name='email']")).setValue("test@tets.xk");
-        $(byXpath("//input[@class='input__default' and @name='password']")).setValue("testtetsxk");
-        $(byId("send-login")).click();
-
-        Assert.assertEquals($(byXpath("//div[@class='alert alert-danger']/ul/li")).getText(), "Неправильный e-mail или пароль!");
-
+        page.navigate("https://chitatel.by/");
+        page.locator(
+                "//div[@class='h-login']//a[@class='block__link login__link login_popup']//div[@class ='name']")
+                .click();
+        page.locator("//a[contains(@class,'_js-tab-btn') and normalize-space()='Email']").click();
+        page.locator("//input[@type='email' and @name='email']").fill("test@tets.xk");
+        page.locator("//input[@class='input__default' and @name='password']").fill("testtetsxk");
+        page.locator("//input[@id='send-login']").click();
+        Assert.assertEquals(page.locator("//div[@class='alert alert-danger']/ul/li").textContent(),
+                "Неправильный e-mail или пароль!");
     }
-
 }
